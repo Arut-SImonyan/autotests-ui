46cb528141a08d4d6bb6c07a99f97861eaea4490
Arut-Simonyan <mightbedeadbytomorrow@gmail.com>
2025-06-19 19:04:25 +0300

    Фикстуры Pytest. Практика работы с pytest фикстурами

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from playwright.sync_api import Playwright, Page
+
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    email_input = page.locator('//*[@id=":r0:"]')
+    email_input.fill("user.name@gmail.com")
+
+    username_input = page.locator('//*[@id=":r1:"]')
+    username_input.fill("username")
+
+    password_input = page.locator('//*[@id=":r2:"]')
+    password_input.fill("password")
+
+    registration_button = page.get_by_test_id("registration-page-registration-button")
+    registration_button.click()
+
+    context.storage_state(path="browser-state.json")
+
+    context.close()
+    browser.close()
+
+@pytest.fixture()
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+    yield page
