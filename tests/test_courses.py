b54e376171ee3055194108adc8530807ab859398
Arut-Simonyan <mightbedeadbytomorrow@gmail.com>
2025-06-26 10:55:17 +0300

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -6,7 +6,6 @@ from pages.create_course_page import CreateCoursePage
 
 
 @pytest.mark.courses
-@pytest.mark.regression
 def test_empty_courses_list(courses_list_page_with_state: CoursesListPage):
 
     courses_list_page_with_state.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
@@ -23,18 +22,20 @@ def test_empty_courses_list(courses_list_page_with_state: CoursesListPage):
 def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
 
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
-    create_course_page.check_visible_create_course_title()
-    create_course_page.check_disabled_create_course_button()
+
+    create_course_page.create_course_toolbar_view.check_visible()
     create_course_page.check_visible_image_preview_empty_view()
     create_course_page.check_visible_image_upload_view()
-    create_course_page.check_visible_create_course_form(title="", description="", estimated_time="", max_score="0", min_score="0")
-    create_course_page.check_visible_exercises_title()
-    create_course_page.check_visible_create_exercise_button()
-    create_course_page.check_visible_exercises_empty_view()
-    create_course_page.upload_preview_image("./testdata/files/image.png")
-    create_course_page.check_visible_image_upload_view()
-    create_course_page.fill_create_course_form(title = "Playwright", estimated_time = "2 weeks", description = "Playwright", max_score = "100", min_score = "10")
-    create_course_page.click_create_course_button()
+
+    create_course_page.create_course_exercises_toolbar_view.check_visible()
+    create_course_page.create_course_exercises_toolbar_view.click_create_exercise_button()
+
+    create_course_page.upload_preview_image("D:/autotests-ui/testdata/files/image.png")
+
+    create_course_page.create_course_form.fill(title = "Playwright", estimated_time = "2 weeks", description = "Playwright", max_score = "100", min_score = "10")
+    create_course_page.create_course_form.check_visible(title = "Playwright", estimated_time = "2 weeks", description = "Playwright", max_score = "100", min_score = "10")
+
+    create_course_page.create_course_toolbar_view.click_create_course_button()
 
     courses_list_page.check_visible_courses_title()
     courses_list_page.check_visible_create_course_button()
--- tests/test_successful_registration.py
+++ tests/test_successful_registration.py
@@ -1,11 +1,11 @@
-import pytest
-
 from pages.registration_page import RegistrationPage
 from pages.dashboard_page import DashboardPage
+import pytest
 
-@pytest.mark.parametrize("email, username, password", [("test@email.com", "username", "password")])
-def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage, email: str, username: str, password: str):
+
+@pytest.mark.regression
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
+
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.fill_registration_form(email=email, username=username, password=password)
+    registration_page.registration_form.fill(email="test@email.com", username="username", password="password")
     registration_page.click_registration_button()
-    dashboard_page.check_visible_dashboard_title()
